d67938d8c98ea1302678b9ff11ef0a03c4c9581a
Micah Geisel <micah@botandrose.com>
2020-02-04 11:22:35 -0800

    upgrade dependencies and drop support for older rubies.

--- lib/database_cleaner/neo4j.rb
+++ lib/database_cleaner/neo4j.rb
@@ -1,5 +1,5 @@
 require "database_cleaner/neo4j/version"
-require "database_cleaner"
+require "database_cleaner/core"
 require "database_cleaner/neo4j/transaction"
 require "database_cleaner/neo4j/truncation"
 require "database_cleaner/neo4j/deletion"
